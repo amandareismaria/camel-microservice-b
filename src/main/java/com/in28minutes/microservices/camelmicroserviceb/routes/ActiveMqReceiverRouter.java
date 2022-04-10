e9a13155d81f1e70dcc29bf2a1699b06083fe973
Amanda Reis <amandareismaria@gmail.com>
2022-04-10 18:13:16 -0300

    Unmarshalling JSON Message to Java Bean in ActiveMQ Camel Route

--- src/main/java/com/in28minutes/microservices/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
+++ src/main/java/com/in28minutes/microservices/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
@@ -1,15 +1,25 @@
 package com.in28minutes.microservices.camelmicroserviceb.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
+
+import com.in28minutes.microservices.camelmicroserviceb.CurrencyExchange;
 
 @Component
 public class ActiveMqReceiverRouter extends RouteBuilder{
 	
 	public void configure() throws Exception {
 		
+		//JSON
+		//CurrencyExchange
+		//{"id": 1000, "from": "USD","to": "INR", "conversionMultiple": 70}
+		
 		from("activemq:my-activemq-queue")
+		.unmarshal().json(JsonLibrary.Jackson,CurrencyExchange.class)
 		.to("log:received-message-from-active-mq");
 	}
 
 }
+
+
